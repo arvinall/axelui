beda66079e1830688f4800e1ef37ed7cce3f5b8f
arvinall <arvin@ladan.org>
2013-06-13 03:39:29 +0430

    only giv one password

--- unistall.sh
+++ unistall.sh
@@ -3,26 +3,40 @@ echo '1'
 echo '#unistalling wizard'
 zenity --question --title='Remove ?' --text='Do you want to un-install the axelui ?'
 if [[ $? = 1 ]] ; then
-	zenity --info --text='plZ enter your password for remove'
 	exit
 fi
-
+	pass=`zenity --password --title='plZ enter your password for remove'`
+	if [ $? != 0 ]; then
+		(
+		echo `date`
+		echo 'For install axel UI you need to have root Permission'
+		echo 'Plz enter your password in wizard'
+		echo ''
+		) >> log
+		echo 'find a problem Plz read log file'
+		exit
+	fi
 (
 	echo '25'
 	echo '#Remove axelfast'
-       gksudo rm /usr/bin/axelfast
+       echo $pass | sudo -S rm /usr/bin/axelfast
+		sleep 0.5
 	echo '50'
 	echo '#Remove axelui'
-       gksudo rm /usr/bin/axelui
+       echo $pass | sudo -S rm /usr/bin/axelui
+		sleep 0.5
 	echo '75'
 	echo '#Remove urlentry.info'
-       gksudo rm /usr/share/doc/axelui/urlentry.info
+       echo $pass | sudo -S rm /usr/share/doc/axelui/urlentry.info
+		sleep 0.5
 	echo '80'
 	echo '#Remove license'
-       gksudo rm /usr/share/doc/axelui/license && gksudo rmdir /usr/share/doc/axelui
+       echo $pass | sudo -S rm /usr/share/doc/axelui/license && echo $pass | sudo -S rmdir /usr/share/doc/axelui
+		sleep 0.5
 	echo '99'
 	echo '#Remove axelui.desktop (shortcut)'
-       gksudo rm /usr/share/applications/axelui.desktop
+       echo $pass | sudo -S rm /usr/share/applications/axelui.desktop
+		sleep 0.5
 	echo '100'
 	echo '#Unistall finished'
 ) | zenity --progress --no-cancel --width=400 --title='removing'
